78a4a3d618afdf64dcdb6237fe359d7dfcfa95fd
iuaks <maksimpagor99@gmail.com>
2025-04-29 16:42:08 +0300

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -6,17 +6,14 @@
 
 Person::Person() : name("Unknown"), age(0) {}
 Person::Person(const std::string& n, int a) : name(n), age(a) {}
-Person::Person(const Person& other) : name(other.name), age(other.age) {}
-Person::Person(Person&& other) noexcept : name(std::move(other.name)), age(other.age) {}
-
-Person::~Person() {}
-
-void Person::setName(const std::string& n) { name = n; }
-std::string Person::getName() const { return name; }
-
-void Person::setAge(int a) { age = a; }
-int Person::getAge() const { return age; }
+Person::~Person() {
+    std::cout << "Person destructor called\n";
+}
 
 void Person::display() const {
     std::cout << "Name: " << name << ", Age: " << age;
+}
+
+std::string Person::getRole() const {
+    return "Person";
 }
